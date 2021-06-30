a47c0ebf75f1ad871f2b2790ac26cd5745ceb01c
Dan <bmdmartinez@gmail.com>
2021-06-30 15:15:07 -0400

    Display API data in div, working on icon

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,15 +3,38 @@ var apiKey = "588cb54af9e39e64a535d1bc49b2842f";
 
 function dailyWeather() {
     var searchCity = document.querySelector("#city-search").value;
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey)
-    .then(function(response){
-        return response.json();
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "")
+    .then(function(oneDay){
+        return oneDay.json();
     })
-    .then(function(response){
-        var currentTemp = parseInt((response.main.temp) * 9/5 - 459);
-        var currentHumidity = response.main.humidity;
-        var windSpeed = response.wind.speed;
-        // var uvIndex = response.current.uvi;
-        console.log(currentTemp, currentHumidity, windSpeed);
+    .then(function(oneDay){
+        var cityName = oneDay.name;
+        var currentTemp = "Current Temperature: " + parseInt((oneDay.main.temp) * 9/5 - 459) + "°F";
+        var currentHumidity = "Humidity: " + oneDay.main.humidity + "%";
+        var windSpeed = "Wind Speed: " + oneDay.wind.speed + " MPH";
+        var weatherIcon = " <img src=" + oneDay.weather[0].icon + ".png";
+        // var uvIndex = oneDay.current.uvi;
+        var showCity = document.querySelector("#cityName");
+        var showDailyTemp = document.querySelector("#currentTemp");
+        var showDailyHumidity = document.querySelector("#currentHumidity");
+        var showDailyWind = document.querySelector("#currentWind");
+        showCity.innerHTML = cityName + weatherIcon;
+        showDailyTemp.innerHTML = currentTemp;
+        showDailyHumidity.innerHTML = currentHumidity;
+        showDailyWind.innerHTML = windSpeed;
+
+        var cityLat = oneDay.coord.lat;
+        var cityLon = oneDay.coord.lon;
+
+        fetch("https://api.openweathermap.org/data/2.5/onecall?appid="+ apiKey + "&lat=" + cityLat +"&lon=" + cityLon)
+
+        .then(function(oneDayUVI){
+            return oneDayUVI.json();
+        })
+        .then(function(oneDayUV){
+            var uvIndex = "UV Index: " + oneDayUV.current.uvi;
+            var showUV = document.querySelector("#currentUV");
+            showUV.innerHTML = uvIndex;
+        })
     })
 }
