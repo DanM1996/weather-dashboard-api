fc39427afecaa496d384e5e83df2ec4528b585b1
Dan <bmdmartinez@gmail.com>
2021-07-02 12:21:32 -0400

    5 day forecast displays on the webpage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,23 @@
-var citiesArr = [];
+var weatherArr = [];
 var fiveDayContainerEl = document.querySelector("#five-day-container");
+var forecastDay1 = document.querySelector("#day-one");
+var forecastDay2 = document.querySelector("#day-two");
+var forecastDay3 = document.querySelector("#day-three");
+var forecastDay4 = document.querySelector("#day-four");
+var forecastDay5 = document.querySelector("#day-five");
 
 var apiKey = "588cb54af9e39e64a535d1bc49b2842f";
 
 function dailyWeather() {
     var searchCity = document.querySelector("#city-search").value;
-    var oneDayApi = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey; 
+    var oneDayApi = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=imperial"; 
     fetch(oneDayApi)
     .then(function(oneDay){
         return oneDay.json();
     })
     .then(function(oneDay){
         var cityName = oneDay.name;
-        var currentTemp = "Current Temperature: " + parseInt((oneDay.main.temp) * 9/5 - 459) + "°F";
+        var currentTemp = "Current Temperature: " + (oneDay.main.temp) + "°F";
         var currentHumidity = "Humidity: " + oneDay.main.humidity + "%";
         var windSpeed = "Wind Speed: " + oneDay.wind.speed + " MPH";
         var showCity = document.querySelector("#cityName");
@@ -24,9 +29,11 @@ function dailyWeather() {
         showDailyHumidity.innerHTML = currentHumidity;
         showDailyWind.innerHTML = windSpeed;
 
+        // logging the lat and lon of the city entered for uv index data
         var cityLat = oneDay.coord.lat;
         var cityLon = oneDay.coord.lon;
 
+        // seperate api to capture UV Index data
         fetch("https://api.openweathermap.org/data/2.5/onecall?appid="+ apiKey + "&lat=" + cityLat +"&lon=" + cityLon)
 
         .then(function(oneDayUVI){
@@ -37,21 +44,43 @@ function dailyWeather() {
             var showUV = document.querySelector("#currentUV");
             showUV.innerHTML = uvIndex;
         })
-    })
+    }) 
+    // call forecast function after the first day finishes
     forecast();
 }
 
 function forecast() {
     var searchCity = document.querySelector("#city-search").value;
-    var fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + apiKey;
+    var fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + apiKey + "&units=imperial";
     fetch(fiveDayApi)
     .then(function(fiveDay){
         return fiveDay.json();
     })
     .then(function(fiveDay){
-        console.log(fiveDay.list[0].main.temp, fiveDay.list[1].main.temp);
-        console.log(fiveDay)
-        console.log(fiveDay.list[0].main.humidity, fiveDay.list[0].wind.speed);
+        
+        // create variables to log the data of temp, humidity, and wind speed for 5 days
+        var day1Temp = "Temp: " + fiveDay.list[1].main.temp;
+        var day1Humidity = " Humitidy: " + fiveDay.list[1].main.humidity
+        var day1Wind = " Wind Speed: " + fiveDay.list[1].wind.speed; 
+        var day2Temp = "Temp: " + fiveDay.list[2].main.temp;
+        var day2Humidity = " Humitidy: " + fiveDay.list[2].main.humidity
+        var day2Wind = " Wind Speed: " + fiveDay.list[2].wind.speed;
+        var day3Temp = "Temp: " + fiveDay.list[3].main.temp;
+        var day3Humidity = " Humitidy: " + fiveDay.list[3].main.humidity
+        var day3Wind = " Wind Speed: " + fiveDay.list[3].wind.speed;
+        var day4Temp = "Temp: " + fiveDay.list[4].main.temp;
+        var day4Humidity = " Humitidy: " + fiveDay.list[4].main.humidity
+        var day4Wind = " Wind Speed: " + fiveDay.list[4].wind.speed;
+        var day5Temp = "Temp: " + fiveDay.list[5].main.temp;
+        var day5Humidity = " Humitidy: " + fiveDay.list[5].main.humidity
+        var day5Wind = " Wind Speed: " + fiveDay.list[5].wind.speed;
+
+        // display the 5 day forecast in the HTML
+        forecastDay1.innerHTML = day1Temp + day1Humidity + day1Wind;
+        forecastDay2.innerHTML = day2Temp + day2Humidity + day2Wind;
+        forecastDay3.innerHTML = day3Temp + day3Humidity + day3Wind;
+        forecastDay4.innerHTML = day4Temp + day4Humidity + day4Wind;
+        forecastDay5.innerHTML = day5Temp + day5Humidity + day5Wind;
     })
     
 }
